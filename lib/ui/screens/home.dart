47d2396d1508bd21ee33dc7469129a67b66c4a4c
Andriy S'omak <semack@gmail.com>
2021-02-08 15:09:56 +0200

    replaced font

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,12 +1,10 @@
-import 'dart:io';
 
 import 'package:colour/colour.dart';
-import 'package:filesystem_picker/filesystem_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:zx_tape_player/ui/screens/player.dart';
 import 'package:zx_tape_player/ui/screens/search.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -19,16 +17,16 @@ class HomeScreen extends StatelessWidget {
             child: Column(children: <Widget>[
       Padding(
           padding: EdgeInsets.fromLTRB(0, 100.0, 0, 0),
-          child: Text('Find the desired tape',
+          child: Text(tr('find_tape'),
               textAlign: TextAlign.center,
               style: TextStyle(
-                  fontSize: 12.0,
+                  fontSize: 16.0,
                   color: Theme.of(context).primaryColor))),
       Padding(
         padding: EdgeInsets.fromLTRB(16, 24.0, 16, 0),
         child: TextField(
           controller: _controller,
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontSize: 18.0, letterSpacing: -0.5,),
           autofocus: true,
           onChanged: (text) {
             if (text.isNotEmpty) {
@@ -40,17 +38,20 @@ class HomeScreen extends StatelessWidget {
           cursorColor: Colors.white,
           decoration: InputDecoration(
             border: InputBorder.none,
-            hintText: 'Type a search text here',
+
+            hintText: tr('search_hint'),
             filled: true,
             fillColor: Colour('#28384C'),
             isDense: true,
-            prefixIconConstraints: BoxConstraints(
-              minWidth: 24,
-              minHeight: 24),
+            prefixIconConstraints: BoxConstraints(minWidth: 24, minHeight: 24),
             prefixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Image.asset('assets/images/home/search-icon.png')),
-            hintStyle: TextStyle(fontSize: 12.0, color: Colour('546B7F')),
+            hintStyle: TextStyle(
+              fontSize: 16.0,
+              color: Colour('546B7F'),
+              letterSpacing: -0.5,
+            ),
             contentPadding: EdgeInsets.symmetric(vertical: 16.0),
             focusedBorder: UnderlineInputBorder(
               borderSide: BorderSide(color: Colors.transparent, width: 0.0),
@@ -65,11 +66,10 @@ class HomeScreen extends StatelessWidget {
       ),
       Padding(
           padding: EdgeInsets.fromLTRB(0, 65.0, 0, 0),
-          child: Text('or select from file',
+          child: Text(tr('select_file'),
               textAlign: TextAlign.center,
               style: TextStyle(
-                  fontFamily: 'ZxSpectrum',
-                  fontSize: 12.0,
+                  fontSize: 16.0,
                   color: Theme.of(context).primaryColor))),
       Padding(
         padding: EdgeInsets.fromLTRB(0, 24.0, 0, 0),
@@ -78,36 +78,21 @@ class HomeScreen extends StatelessWidget {
           textColor: Theme.of(context).primaryColor,
           padding: EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 16.0),
           onPressed: () async {
-            var rootPath = await getTemporaryDirectory();
-
-            var filePath = await FilesystemPicker.open(
-              rootDirectory: rootPath,
-              title: 'Open file',
-              context: context,
-              fsType: FilesystemType.folder,
-              allowedExtensions: ['tap', 'tzx'],
-              folderIconColor: Colors.teal,
-              requestPermission: () async =>
-              await Permission.storage.request().isGranted,
+            FilePickerResult result = await FilePicker.platform.pickFiles(
+              allowMultiple: false,
+              type: FileType.custom,
+              allowedExtensions: [
+                'tap',
+                'tzx',
+              ],
             );
-            
-            // var filePath = await FilesystemPicker.open(
-            //   title: 'Open file',
-            //   context: context,
-            //   rootDirectory: Directory(dir),
-            //   fsType: FilesystemType.file,
-            //   folderIconColor: Colors.teal,
-            //     allowedExtensions: ['tap', 'tzx'],
-            //   fileTileSelectMode: FileTileSelectMode.wholeTile,
-            // );
 
-            if (filePath.isNotEmpty)
+            if (result.files != null && result.files.length > 0)
               Navigator.pushNamed(context, PlayerScreen.routeName,
-                  arguments: filePath);
+                  arguments: result.files.first);
           },
-                  child: Text(
-                    "SELECT FROM FILES",
-                    style: TextStyle(fontSize: 10.0),
+                  child: Text(tr('select_from_files'),
+                    style: TextStyle(fontSize: 14.0),
                   ),
                 ),
               )
--- lib/ui/screens/search.dart
+++ lib/ui/screens/search.dart
@@ -1,9 +1,9 @@
 import 'package:colour/colour.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:zx_tape_player/services/backend.dart';
-import 'package:zx_tape_player/ui/screens/home.dart';
 import 'package:zx_tape_player/ui/screens/player.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -13,7 +13,8 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextEditingController _controller = TextEditingController();
     _controller.text = ModalRoute.of(context).settings.arguments;
-
+    _controller.selection = TextSelection.fromPosition(
+        TextPosition(offset: _controller.text.length));
     return Scaffold(
         body: Column(
       children: <Widget>[
@@ -22,21 +23,39 @@ class SearchScreen extends StatelessWidget {
           child: TypeAheadField(
             textFieldConfiguration: TextFieldConfiguration(
               controller: _controller,
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(
+                  color: Colors.white, fontSize: 18.0, letterSpacing: -0.5),
               autofocus: true,
               cursorColor: Colors.white,
               onChanged: (text) {
-                if (text.isEmpty)
-                  Navigator.pop(context);
+                if (text.isEmpty) Navigator.pop(context);
               },
               decoration: InputDecoration(
                 border: InputBorder.none,
-                hintText: 'Type a search text here',
+                // suffix: Row(
+                //   mainAxisAlignment: MainAxisAlignment.end,
+                //   mainAxisSize: MainAxisSize.min,
+                //   children: [
+                //     Icon(Icons.keyboard_arrow_right),
+                //     SizedBox(width: 8),
+                //     Icon(Icons.keyboard_arrow_right),
+                //   ],
+                // ),
+                hintText: tr('search_hint'),
                 filled: true,
                 fillColor: Colour('#28384C'),
-                prefixIcon: Image.asset('assets/images/home/search-icon.png'),
-                hintStyle: TextStyle(fontSize: 12.0, color: Colour('546B7F')),
-                contentPadding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                isDense: true,
+                prefixIconConstraints:
+                    BoxConstraints(minWidth: 24, minHeight: 24),
+                prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Image.asset('assets/images/home/search-icon.png')),
+                hintStyle: TextStyle(
+                  fontSize: 16.0,
+                  color: Colour('546B7F'),
+                  letterSpacing: -0.5,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 16.0),
                 focusedBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: Colors.transparent, width: 0.0),
                   borderRadius: BorderRadius.circular(3.5),
@@ -50,14 +69,49 @@ class SearchScreen extends StatelessWidget {
             suggestionsCallback: (pattern) async {
               return await BackendService.getSuggestions(pattern);
             },
+            suggestionsBoxDecoration: SuggestionsBoxDecoration(
+              hasScrollbar: false,
+              elevation: 0,
+              constraints: BoxConstraints(minWidth: 0, minHeight: 0),
+              shadowColor: Colors.transparent,
+              color: Colour('#546B7F'),
+            ),
+            noItemsFoundBuilder: (BuildContext context) {
+              return Center(
+                child: Text(
+                  tr('no_items_found'),
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colour('#AFB6BB'),
+                      letterSpacing: -0.5),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            },
+            loadingBuilder: (BuildContext context) {
+              return Center(
+                child: Text(
+                  tr('loading'),
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colour('#AFB6BB'),
+                      letterSpacing: -0.5),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            },
             itemBuilder: (context, suggestion) {
               return ListTile(
-                // key: Key(item.entryId),
-                trailing: Icon(Icons.chevron_right),
-                title: Text(suggestion.text, style: TextStyle(fontSize: 10.0,
-              height: 1.8,
-              letterSpacing:0.4,
-              )));
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0.00),
+                  // key: Key(item.entryId),
+                  trailing: Text('>', style: TextStyle(color: Colors.white)),
+                  title: Text(suggestion.text,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        height: 1.8,
+                        letterSpacing: -0.5,
+                      )));
             },
             onSuggestionSelected: (suggestion) => Navigator.pushNamed(
                 context, PlayerScreen.routeName,
