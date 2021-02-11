d2d7eaa49a28bbc5e0f78df47f0b4a58231cf644
Andriy S'omak <semack@gmail.com>
2021-02-11 13:12:02 +0200

    Added ios appcenter analytics

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +10,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  AppCenter.start(withAppSecret: "b1cba03b-9d97-40bb-b013-d72470115b6e", services:[
+    Analytics.self,
+    Crashes.self
+  ])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
