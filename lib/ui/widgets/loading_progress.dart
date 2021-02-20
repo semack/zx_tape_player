82ca5e4fd883d5c1d2531033c53b5ecd7906937c
Andriy S'omak <semack@gmail.com>
2021-02-21 00:29:01 +0200

    changed progress indicator

--- lib/ui/widgets/loading_progress.dart
+++ lib/ui/widgets/loading_progress.dart
@@ -1,6 +1,7 @@
 import 'package:colour/colour.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:loading/indicator/ball_pulse_indicator.dart';
+import 'package:loading/loading.dart';
 
 class LoadingProgress extends StatelessWidget {
   LoadingProgress({Key key}) : super(key: key);
@@ -8,12 +9,17 @@ class LoadingProgress extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        tr('loading'),
-        style: TextStyle(
-            fontSize: 14, color: Colour('#AFB6BB'), letterSpacing: -0.5),
-        textAlign: TextAlign.center,
-      ),
+      child:           Loading(indicator: BallPulseIndicator(), size: 30.0, color: Colour('#AFB6BB')),
+      // child: Row(
+      //   children: [
+      //     Text(
+      //       tr('loading'),
+      //       style: TextStyle(
+      //           fontSize: 14, color: Colour('#AFB6BB'), letterSpacing: -0.5),
+      //       textAlign: TextAlign.center,
+      //     ),
+      //   ],
+      // )
     );
   }
 }
