ec24e98d9f0e14f870b77ad09d87ddc836beea08
Andriy S'omak <semack@gmail.com>
2021-03-23 08:13:01 +0200

    103

--- lib/ui/player_screen.dart
+++ lib/ui/player_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:app_center_bundle_sdk/app_center_bundle_sdk.dart';
@@ -228,7 +229,7 @@ Widget _buildInfoWidget(
                                   letterSpacing: 0.3,
                                   fontSize: 12.0),
                             ),
-                            SizedBox(width: 8),
+                            SizedBox(width: 20),
                             Icon(
                               Icons.star_rounded,
                               color: HexColor('#B1B8C1'),
@@ -244,23 +245,27 @@ Widget _buildInfoWidget(
                                   letterSpacing: 0.3,
                                   fontSize: 12.0),
                             ),
+                            SizedBox(width: 20),
+                            Platform.isIOS
+                                ? SizedBox.shrink()
+                                : Icon(
+                                    Icons.account_balance_wallet_rounded,
+                                    color: HexColor('#B1B8C1'),
+                                    size: 12.0,
+                                  ),
                             SizedBox(width: 8),
-                            Icon(
-                              Icons.account_balance_wallet_rounded,
-                              color: HexColor('#B1B8C1'),
-                              size: 12.0,
-                            ),
-                            SizedBox(width: 5.0),
-                            Text(
-                              model.price.isNullOrEmpty()
-                                  ? tr('na')
-                                  : model.price,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  letterSpacing: 0.3,
-                                  fontSize: 12.0),
-                              overflow: TextOverflow.ellipsis,
-                            ),
+                            Platform.isIOS
+                                ? SizedBox.shrink()
+                                : Text(
+                                    model.price.isNullOrEmpty()
+                                        ? tr('na')
+                                        : model.price,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        letterSpacing: 0.3,
+                                        fontSize: 12.0),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                           ],
                         ),
                         model.remarks.isNullOrEmpty()
